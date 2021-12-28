bd69fe1cec397ea00d0be30f94deced35d97e11f
Sasha <playakkayntsani@mail.ru>
2021-12-28 16:04:27 +0300

    Fist Practic

--- js/script.js
+++ js/script.js
@@ -1,22 +1,43 @@
 'use strict';
 
-console.log('arr' + ' - object');
-console.log(4 +'5');
+// console.log('arr' + ' - object');
+// console.log(4 +'5');
 
-let incr = 10,
-    decr = 10;
+// let incr = 10,
+//     decr = 10;
 
-// ++incr;
-// --decr;
+// // ++incr;
+// // --decr;
 
-console.log(++incr);
-console.log(--decr);
+// console.log(++incr);
+// console.log(--decr);
 
-console.log(5%2);
+// console.log(5%2);
 
-console.log(2+2*2 !== '6');
+// console.log(2+2*2 !== '6');
 
-const isChecked = false,
-    isClose = false;
+// const isChecked = false,
+//     isClose = false;
 
-console.log(isChecked || !isClose);
+// console.log(isChecked || !isClose);
+
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже просмотрели?", " ");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false 
+};
+
+const a = prompt ("Один из последниз просмотренных фильмов", ""),
+      b = prompt ("На сколько цените его?", ""),
+      c = prompt ("Один из последних просмотренных фильмов?", ""),
+      d = prompt ("На сколько цените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
